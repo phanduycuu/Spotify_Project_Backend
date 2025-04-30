bf9a52b3127bb4f44a3155ddec664f1f7e1a3d43
phanduycuu <0cauchin0@gmail.com>
2025-04-30 23:09:39 +0700

    fix media

--- Spotify_Project_Backend/settings.py
+++ Spotify_Project_Backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -168,3 +168,6 @@ CHANNEL_LAYERS = {
         # },
     },
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
