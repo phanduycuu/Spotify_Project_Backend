58086972ee776dab3090ff83823dbf6176ef9b91
phanduycuu <0cauchin0@gmail.com>
2025-05-01 10:13:19 +0700

    get song byAlbumID

--- album/views.py
+++ album/views.py
@@ -2,7 +2,10 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Album
 from .serializers import AlbumSerializer
-
+from song.models import Song
+from song.serializers import SongReadSerializer
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
@@ -16,5 +19,15 @@ class AlbumViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response({
             'count': queryset.count(),
+            'results': serializer.data
+        }, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'], url_path='get-songs/(?P<album_id>[^/.]+)')
+    def get_songs(self, request, album_id=None):
+        album = get_object_or_404(Album, pk=album_id, is_deleted=False)
+        songs = album.album_songs.filter(is_deleted=False)
+        serializer = SongReadSerializer(songs, many=True)
+        return Response({
+            'count': songs.count(),
             'results': serializer.data
         }, status=status.HTTP_200_OK)
