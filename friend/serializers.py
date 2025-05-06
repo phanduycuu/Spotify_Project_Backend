cee09e4ed0c1081abbe940d81bb732009e79f9ef
cửu phan duy <0cauchin0@gmail.com>
2025-05-06 20:56:59 +0700

    fix friend

--- friend/serializers.py
+++ friend/serializers.py
@@ -1,7 +1,14 @@
 # project/serializers.py
 from rest_framework import serializers
 from friend.models import Friend
+from account.models import Account
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['id', 'email', 'full_name', 'sex', 'birthday']
 class FriendSerializer(serializers.ModelSerializer):
+    sender = AccountSerializer(source='user1')
     class Meta:
         model = Friend
         fields = '__all__'
